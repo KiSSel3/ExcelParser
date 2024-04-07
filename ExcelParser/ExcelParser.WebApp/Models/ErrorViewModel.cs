4c250049b1ffc05314b0300cf1ff3eded1ce9eb4
Alexey-beercode <alsemkovbn@gmail.com>
2024-04-07 16:25:41 +0300

    Added web app

---
+++ ExcelParser/ExcelParser.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExcelParser.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
